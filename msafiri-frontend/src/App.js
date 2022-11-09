3fcd85cb1de51c440eb9f29e7fd3dfd52a170799
Linah Imbari <linahmuhonjaimbari@gmail.com>
2022-11-09 08:43:23 +0300

    created the login component

---
+++ msafiri-frontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Login from './components/Login'
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "bootstrap-css-only/css/bootstrap.min.css";
+import "mdbreact/dist/css/mdb.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Login />     
+    </div>
+  );
+}
+
+export default App;
